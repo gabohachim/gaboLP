f030338c07454c9e2b279ce7d8aad7d50245414e
gabohachim <gabo.hachim@gmail.com>
2025-12-14 18:22:13 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
+import 'package:flutter/material.dart';
+import 'ui/home_screen.dart';
+
 void main() {
   runApp(const GaBoLpApp());
 }
 
+class GaBoLpApp extends StatelessWidget {
+  const GaBoLpApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Colección vinilos',
+      theme: ThemeData(useMaterial3: true),
+      home: const HomeScreen(),
+    );
+  }
+}
